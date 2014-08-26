b053d75b7a5825d08e25747e3321819061b61b9f
Haralan Dobrev <hkdobrev@gmail.com>
2014-08-27 00:35:39 +0300

    Do not use debug output for exceptions on production environment

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 
+namespace Timezones;
+
 // Use Composer autoloader
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\Debug\ErrorHandler;
-use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\Debug;
 
 // Initialize the application
 $app = require __DIR__.'/../src/app.php';
@@ -12,16 +13,19 @@ $app = require __DIR__.'/../src/app.php';
 // Load environment and relevant configuration
 $environment = getenv('TIMEZONES_ENV') ?: 'prod';
 require __DIR__.'/../config/'.$environment.'.php';
-unset($environment);
 
 // http://silex.sensiolabs.org/doc/cookbook/error_handler.html
-ErrorHandler::register();
-ExceptionHandler::register($app['debug']);
+Debug\ErrorHandler::register();
+Debug\ExceptionHandler::register(
+    'prod' === $environment ? false : $app['debug']
+);
+
+unset($environment);
 
 // Include the controllers
 require __DIR__.'/../src/controllers.php';
 
 // Run the app
 // create an http foundation request implementing OAuth2\RequestInterface
-$request = OAuth2\HttpFoundationBridge\Request::createFromGlobals();
+$request = \OAuth2\HttpFoundationBridge\Request::createFromGlobals();
 $app->run($request);
