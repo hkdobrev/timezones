a901437844a42a35e58f1543deede13e7ab7edd5
Haralan Dobrev <hkdobrev@gmail.com>
2014-08-27 00:41:07 +0300

    Redirect unmatched paths to home.
    The frontend routes of AngularJS are not recognized on the backend.
    Until now this will result in a 404 page.
    If you go to such frontend page (e.g. /signup) and refresh the page,
    you would see a 404 on the same URI where you have just seen some content.
    
    When redirecting to the homepage so AngularJS could load and present content,
    we are also including the previous path in the fragment identifier.
    
    AngularJS could recognize it and display the relevant view and controller.
    If it does not recognize it as well it will "redirect" to its default route.

--- src/controllers.php
+++ src/controllers.php
@@ -2,6 +2,26 @@
 
 namespace Timezones;
 
-$app->mount('/', new ControllerProvider());
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
+
+$app['base_path'] = '/';
+
+$app->mount($app['base_path'], new ControllerProvider());
 $app->mount('/auth', new OAuth2\Client\OAuth2ClientControllerProvider());
 $app->mount('/oauthserver', new OAuth2\Server\OAuth2ServerControllerProvider());
+
+// Unmatched routes should go to the homepage so AngularJS could load.
+// AngularJS front-end routes are not currently matched on the back-end.
+$app->error(function(NotFoundHttpException $exception, $code) use ($app) {
+    if (!$exception->getPrevious()
+     or ! $exception->getPrevious() instanceof ResourceNotFoundException) {
+        return;
+    }
+
+    return new Response('', 302, [
+        // Include the previous path in the fragment so AngularJS could use it
+        'location' => $app['base_path'].'#'.$_SERVER['REQUEST_URI'],
+    ]);
+});
